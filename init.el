e3cb5ecc2158053f5a6ede01ad75fab1352948df
Xuan MA <SuperMMX@gmail.com>
2022-06-04 18:25:23 +1000

    Add init-defaults

--- init.el
+++ init.el
@@ -32,19 +32,8 @@
 
 (add-subdirs-to-load-path ".")
 
-;; 禁用工具栏
-(tool-bar-mode -1)
-;; 禁用菜单栏
-(menu-bar-mode -1);
-;; 禁用滚动条
-(scroll-bar-mode -1)
-
-(setq
- ;; 不要缩放frame.
- frame-inhibit-implied-resize t
- ;; 默认用最简单的模式
- initial-major-mode 'fundamental-mode
-)
+;; 缺省
+(require 'init-defaults)
 
 ;; 包管理
 (require 'init-package-management)
