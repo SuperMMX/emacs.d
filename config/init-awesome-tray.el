9c285923a77b00f51fe8a5deec84e9d1eb609ba3
Xuan MA <SuperMMX@gmail.com>
2022-06-04 22:11:29 +1000

    Add more awesome-tray modules

--- config/init-awesome-tray.el
+++ config/init-awesome-tray.el
@@ -1,13 +1,19 @@
 (require 'awesome-tray)
 
+;; Meow 状态
 (defun awesome-tray-module-meow-info ()
   (string-trim (meow-indicator)))
 
 (add-to-list 'awesome-tray-module-alist
              '("meow" . (awesome-tray-module-meow-info awesome-tray-module-evil-face)))
 
+;; 修改
+(setq awesome-tray-buffer-name-buffer-changed t)
+;; 只读
+(setq awesome-tray-buffer-read-only-style "%")
+
 (setq awesome-tray-active-modules
-      '("buffer-name" "belong" "location" "mode-name" "meow" "date"))
+      '("git" "buffer-read-only" "buffer-name" "belong" "location" "date"))
 
 (awesome-tray-mode 1)
 
