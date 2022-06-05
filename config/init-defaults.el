d297d22157c8a380c362d58e96e12c9ca77a1854
Xuan MA <SuperMMX@gmail.com>
2022-06-05 10:32:36 +1000

    Update scrolling and visual line navigation

--- config/init-defaults.el
+++ config/init-defaults.el
@@ -4,6 +4,8 @@
 (menu-bar-mode -1);
 ;; 禁用滚动条
 (scroll-bar-mode -1)
+;; 长行移动
+(global-visual-line-mode)
 
 (setq
  ;; 不要创建~备份文件
--- config/init-smooth-scrolling.el
+++ config/init-smooth-scrolling.el
@@ -2,7 +2,7 @@
 (use-package smooth-scrolling)
 
 ;; 触发滚动的行数
-(setq smooth-scroll-margin 10)
+(setq smooth-scroll-margin 30)
 
 (smooth-scrolling-mode 1)
 
