b80e4974dc006578aaf58ef17a746d8d734fa969
Xuan MA <SuperMMX@gmail.com>
2022-06-11 08:27:31 +1000

    Add key bindings for lsp

--- config/init-lsp.el
+++ config/init-lsp.el
@@ -4,6 +4,25 @@
 ;; Java
 (require 'lsp-bridge-jdtls)
 
+;; 按键
+
+(one-key-create-menu
+ "LSP"
+ '(
+   (("d" . "Find Def") . lsp-bridge-find-def)
+   (("i" . "Find Impl") . lsp-bridge-find-impl)
+   (("b" . "Find Back") . lsp-bridge-return-from-def)
+   (("r" . "Find Refs") . lsp-bridge-find-references)
+   (("d" . "Open Doc") . lsp-bridge-lookup-documentation)
+   (("R" . "Rename") . lsp-bridge-rename)
+   (("n" . "Next Diag") . lsp-bridge-jump-to-next-diagnostic)
+   (("p" . "Prev Diag") . lsp-bridge-jump-to-prev-diagnostic)
+   (("S" . "Restart") . lsp-bridge-restart-process)
+   )
+ t)
+
+(meow-leader-define-key '("l" . one-key-menu-lsp))
+
 (global-lsp-bridge-mode)
 
 (provide 'init-lsp)
