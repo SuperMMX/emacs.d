028a8ec533b509f1ddfb03736b5a174de801dfa7
Xuan MA <SuperMMX@gmail.com>
2022-06-21 22:17:14 +1000

    Add more dark themes

--- config/init-theme.el
+++ config/init-theme.el
@@ -1,22 +1,56 @@
 (use-package doom-themes)
 
-(defvar mmx-themes '(
-                     doom-city-lights
-                     doom-dark+
-                     doom-gruvbox
-                     doom-Iosvkem
-                     doom-material
-                     doom-molokai
-                     doom-oceanic-next
-                     doom-one
-                     doom-palenight
-                     doom-peacock
-                     doom-snazzy
-                     doom-sourcerer
-                     doom-spacegrey
-                     doom-tomorrow-night
-                     doom-vibrant
-                     ))
+(use-package ample-theme
+  :init 
+  (progn
+    (load-theme 'ample-flat t t)
+    (enable-theme 'ample-flat))
+  )
+(use-package ample-zen-theme)
+(use-package darktooth-theme)
+(use-package gotham-theme)
+(use-package jbeans-theme)
+(use-package lush-theme)
+(use-package mustang-theme)
+(use-package obsidian-theme)
+(use-package planet-theme)
+(use-package soft-charcoal-theme)
+(use-package solarized-theme)
+(use-package tangotango-theme)
+(use-package ujelly-theme)
+
+(defvar mmx-themes 
+  '(
+    ample-flat
+    ample-zen
+    darktooth
+    doom-city-lights
+    doom-dark+
+    doom-gruvbox
+    doom-Iosvkem
+    doom-material
+    doom-molokai
+    doom-oceanic-next
+    doom-one
+    doom-palenight
+    doom-peacock
+    doom-snazzy
+    doom-sourcerer
+    doom-spacegrey
+    doom-tomorrow-night
+    doom-vibrant
+    gotham
+    jbeans
+    lush
+    mustang
+    misterioso
+    obsidian
+    planet
+    soft-charcoal
+    solarized-dark
+    tangotango
+    ujelly
+    ))
 
 ;; Random theme
 (defun random-list-element (arg-list)
@@ -30,7 +64,5 @@
   )
 
 (random-theme)
-
-;;(load-theme 'ample-theme)
 
 (provide 'init-theme)
