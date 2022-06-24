e4718f02940731c0e165abed90b12b08f19ba62d
Xuan MA <SuperMMX@gmail.com>
2022-06-24 22:35:44 +1000

    Add EXT-B font

--- config/init-font.el
+++ config/init-font.el
@@ -21,17 +21,23 @@
 ;; (load-font-setup)
 
 
-(defun set-font (FONT-NAME CN-FONT-NAME &optional INITIAL-SIZE CN-FONT-RESCALE-RATIO)
+(defun set-font (FONT-NAME CN-FONT-NAME EXTB-FONT-NAME &optional INITIAL-SIZE CN-FONT-RESCALE-RATIO EXTB-FONT-RESCALE-RATIO)
   "Set different font-family for Latin and Chinese charactors."
   (let* ((size (or INITIAL-SIZE 14))
 	       (ratio (or CN-FONT-RESCALE-RATIO 0.0))
+	       (extb-ratio (or EXTB-FONT-RESCALE-RATIO CN-FONT-RESCALE-RATIO))
 	       (main (font-spec :name FONT-NAME :size size))
-	       (cn (font-spec :name CN-FONT-NAME)))
+	       (cn (font-spec :name CN-FONT-NAME))
+	       (extb (font-spec :name EXTB-FONT-NAME)))
     (set-face-attribute 'default nil :font main)
-    (dolist (charset '(kana han symbol cjk-misc bopomofo))
+    (dolist (charset '(kana han symbol cjk-misc bopomofo gb18030))
       (set-fontset-font t charset cn))
-    (setq face-font-rescale-alist (if (/= ratio 0.0) `((,CN-FONT-NAME . ,ratio)) nil))))
+    (set-fontset-font t nil extb)
+    (setq face-font-rescale-alist
+          `((,CN-FONT-NAME . ,ratio)
+            (,EXTB-FONT-NAME . ,extb-ratio)))
+    ))
 
-(set-font "Hack" "H-MingLan" 16 1.25)
+(set-font "Hack" "H-MingLan" "中华书局宋体02平面" 16 1.25 1.25)
 
 (provide 'init-font)
