c006812d6952ad5f517b197e15d6038722886b4d
Xuan MA <SuperMMX@gmail.com>
2023-01-11 10:03:06 +1100

    Use builit treesit

--- config/init-tree-sitter.el
+++ config/init-tree-sitter.el
@@ -1,7 +1,25 @@
-(use-package tree-sitter)
-(use-package tree-sitter-langs)
+;; Emacs builtin Tree Sitter
+(use-package treesit
+  :when (and (fboundp 'treesit-available-p)
+             (treesit-available-p))
+  :custom (major-mode-remap-alist
+           '(
+             (c-mode          . c-ts-mode)
+             (c++-mode        . c++-ts-mode)
+             (cmake-mode      . cmake-ts-mode)
+             (conf-toml-mode  . toml-ts-mode)
+             (css-mode        . css-ts-mode)
+             (java-mode       . java-ts-mode)
+             (js-mode         . js-ts-mode)
+             (js-json-mode    . json-ts-mode)
+             (python-mode     . python-ts-mode)
+             (sh-mode         . bash-ts-mode)
+             (typescript-mode . typescript-ts-mode))))
 
-(global-tree-sitter-mode)
-(add-hook 'tree-sitter-after-on-hook #'tree-sitter-hl-mode)
+;;(use-package tree-sitter)
+;;(use-package tree-sitter-langs)
+
+;;(global-tree-sitter-mode)
+;;(add-hook 'tree-sitter-after-on-hook #'tree-sitter-hl-mode)
 
 (provide 'init-tree-sitter)
