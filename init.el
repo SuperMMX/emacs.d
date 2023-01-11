9fa669d918d144ee7aab288ce8f3d50498716ab3
Xuan MA <SuperMMX@gmail.com>
2023-01-11 22:17:53 +1100

    Add the auto generated customs

--- init.el
+++ init.el
@@ -146,3 +146,16 @@
          ))))
 
 (provide 'init)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   '(ivy-rich lush-theme yasnippet groovy-mode obsidian-theme popwin web-mode typescript-mode gotham-theme planet-theme ujelly-theme ample-zen-theme doom-themes darktooth-theme jbeans-theme adoc-mode smooth-scrolling mustang-theme counsel-projectile citre auctex unfill soft-charcoal-theme hl-todo key-chord solarized-theme ample-theme tangotango-theme symbol-overlay magit meow ws-butler markdown-mode posframe)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
