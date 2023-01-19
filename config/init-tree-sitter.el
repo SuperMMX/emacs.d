3bec04af14f6b4563ceada7b87a9f216ac2ad90d
Xuan MA <SuperMMX@gmail.com>
2023-01-20 10:14:58 +1100

    Revert to tree-sitter

--- config/init-tree-sitter.el
+++ config/init-tree-sitter.el
@@ -1,25 +1,28 @@
 ;; Emacs builtin Tree Sitter
-(use-package treesit
-  :when (and (fboundp 'treesit-available-p)
-             (treesit-available-p))
-  :custom (major-mode-remap-alist
-           '(
-             (c-mode          . c-ts-mode)
-             (c++-mode        . c++-ts-mode)
-             (cmake-mode      . cmake-ts-mode)
-             (conf-toml-mode  . toml-ts-mode)
-             (css-mode        . css-ts-mode)
-             (java-mode       . java-ts-mode)
-             (js-mode         . js-ts-mode)
-             (js-json-mode    . json-ts-mode)
-             (python-mode     . python-ts-mode)
-             (sh-mode         . bash-ts-mode)
-             (typescript-mode . typescript-ts-mode))))
+;; (use-package treesit
+;;   :when (and (fboundp 'treesit-available-p)
+;;              (treesit-available-p))
+;;   :custom (major-mode-remap-alist
+;;            '(
+;;              (c-mode          . c-ts-mode)
+;;              (c++-mode        . c++-ts-mode)
+;;              (cmake-mode      . cmake-ts-mode)
+;;              (conf-toml-mode  . toml-ts-mode)
+;;              (css-mode        . css-ts-mode)
+;;              (java-mode       . java-ts-mode)
+;;              (js-mode         . js-ts-mode)
+;;              (js-json-mode    . json-ts-mode)
+;;              (python-mode     . python-ts-mode)
+;;              (sh-mode         . bash-ts-mode)
+;;              (typescript-mode . typescript-ts-mode))))
 
-;;(use-package tree-sitter)
-;;(use-package tree-sitter-langs)
+;; (setq-local treesit-font-lock-level 4)
+;; (treesit-font-lock-recompute-features '(command string variable function operator bracket keyword))
 
-;;(global-tree-sitter-mode)
-;;(add-hook 'tree-sitter-after-on-hook #'tree-sitter-hl-mode)
+(use-package tree-sitter)
+(use-package tree-sitter-langs)
+
+(global-tree-sitter-mode)
+(add-hook 'tree-sitter-after-on-hook #'tree-sitter-hl-mode)
 
 (provide 'init-tree-sitter)
