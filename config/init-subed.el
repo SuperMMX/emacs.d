8406f208267effc0676dcb4b6603d153401c649c
Xuan MA <SuperMMX@gmail.com>
2023-02-12 19:40:57 +1100

    Update subed

--- config/init-subed.el
+++ config/init-subed.el
@@ -5,8 +5,14 @@
   (add-hook 'subed-mode-hook 'subed-disable-sync-point-to-player)
   ;; Remember cursor position between sessions
   (add-hook 'subed-mode-hook 'save-place-local-mode)
-  )
 
-(setq subed-auto-play-media nil)
+  ;; Break lines automatically while typing
+  ;;(add-hook 'subed-mode-hook 'turn-on-auto-fill)
+  ;; Break lines at 40 characters
+  ;;(add-hook 'subed-mode-hook (lambda () (setq-local fill-column 40)))
+
+  (add-to-list 'subed-video-extensions "MPG")
+  (add-to-list 'subed-video-extensions "rm")
+  )
 
 (provide 'init-subed)
