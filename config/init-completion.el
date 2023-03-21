0807f62b2a0a25b82a45c9709a1c1a904b4cba9b
Xuan MA <SuperMMX@gmail.com>
2023-03-21 18:16:50 +1100

    Pinyin for ivy

--- config/init-completion.el
+++ config/init-completion.el
@@ -29,4 +29,41 @@
 ;;   (ivy-posframe-mode 1)
 ;;   )
 
+;;; 让`ivy-read'支持拼音
+(use-package pinyinlib
+    :autoload pinyinlib-build-regexp-string
+    :init
+    (with-no-warnings
+      (defun my-pinyinlib-build-regexp-string (str)
+        "Build a pinyin regexp sequence from STR."
+        (cond ((equal str ".*") ".*")
+              (t (pinyinlib-build-regexp-string str t))))
+
+      (defun my-pinyin-regexp-helper (str)
+        "Construct pinyin regexp for STR."
+        (cond ((equal str " ") ".*")
+              ((equal str "") nil)
+              (t str)))
+
+      (defun pinyin-to-utf8 (str)
+        "Convert STR to UTF-8."
+        (cond ((equal 0 (length str)) nil)
+              ((equal (substring str 0 1) ",")
+               (mapconcat
+                #'my-pinyinlib-build-regexp-string
+                (remove nil (mapcar
+                             #'my-pinyin-regexp-helper
+                             (split-string
+                              (replace-regexp-in-string "," "" str )
+                              "")))
+                ""))
+              (t nil)))
+
+      (defun my-ivy--regex-pinyin (fn str)
+        "The regex builder advice to support pinyin."
+        (or (pinyin-to-utf8 str)
+            (funcall fn str)))
+      (advice-add #'ivy--regex-plus :around #'my-ivy--regex-pinyin)
+      (advice-add #'ivy--regex-ignore-order :around #'my-ivy--regex-pinyin)))
+
 (provide 'init-completion)
